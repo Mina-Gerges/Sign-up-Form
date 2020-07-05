39181a358dffb33951c9d29a3f6684e4c2068662
Mina Gerges <mina.gerges@businessboomers.net>
2020-07-05 11:28:36 +0200

    add error messages + add buttons + change css

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function(event) {
+    
+    // Create a container for validation rule names.
+    var ruleNames = [];
+    var forEach = Array.prototype.forEach;
+    
+    var ruleElements = document.querySelectorAll('[data-rule]');
+    
+    forEach.call(ruleElements, function(element) {
+      var ruleName = element.getAttribute('data-rule');
+      if (ruleNames.indexOf(ruleName) === -1) {
+          ruleNames.push(ruleName);
+      }
+    });
+
