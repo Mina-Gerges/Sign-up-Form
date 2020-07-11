d90dcb071c2a42bdfa7f54f219d440075fe55415
Mina Gerges <mina.gerges@businessboomers.net>
2020-07-11 18:19:56 +0200

    Edit mobile design - add coded with image - center

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// document.addEventListener('DOMContentLoaded', function(event) {
-    
+window.onload = WinLoad;
+
+function WinLoad() {    
     // Create a container for validation rule names.
     var ruleNames = [];
     var forEach = Array.prototype.forEach;
     
     var ruleElements = document.querySelectorAll('[data-rule]');
     
-    forEach.call(ruleElements, function(element) {
+    forEach.call(ruleElements, function(element){ 
       var ruleName = element.getAttribute('data-rule');
       if (ruleNames.indexOf(ruleName) === -1) {
           ruleNames.push(ruleName);
       }
     });
-
+}
