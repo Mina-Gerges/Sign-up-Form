779b002f6a62f1e957ef6f554b4e0933afd3f6a0
Mina Gerges <mina.gerges@businessboomers.net>
2020-07-12 20:02:05 +0200

    Finish validity + edit validity colors in css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.onload = WinLoad;
 
 function WinLoad() {    
+
     // Create a container for validation rule names.
     var ruleNames = [];
     var forEach = Array.prototype.forEach;
@@ -13,4 +14,54 @@ function WinLoad() {
           ruleNames.push(ruleName);
       }
     });
+
+    // Validate all labels 
+    var validate = function() {
+      var messageElements = document.querySelectorAll("label span");
+      
+      forEach.call(messageElements, function(element){
+        element.style.display = "none";
+      });
+
+      document.getElementById('signUpForm').checkValidity();
+  };
+
+  // Validation Fail
+  var validationFail = function(e) {
+    var element, validity;
+    
+    element = e.currentTarget;
+    validity = element.validity;
+
+    if (!validity.valid) {
+      console.log(element, "invalid", validity);
+      ruleNames.forEach(function(ruleName) {
+        checkRule(validity, ruleName, element);
+      });
+
+      e.preventDefault();
+    }
+  };
+
+  var checkRule = function(state, ruleName, element) {  
+    console.log(ruleName, state[ruleName])   ; 
+  if (state[ruleName]) {
+    
+    var rules = element
+                      .nextElementSibling
+                      .querySelectorAll('[data-rule="' + ruleName + '"]');
+    
+    forEach.call(rules, function(rule){
+      rule.style.display = "block";
+    });
+   }
+  };
+  
+  var inputElements = document.querySelectorAll('input:not(button)');
+  forEach.call(inputElements, function(input) {
+      input.oninvalid = validationFail;
+      input.oninput = validate;
+  });
+  
+  document.getElementById('claim').addEventListener('click', validate, false);
 }
